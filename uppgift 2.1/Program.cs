fad729d997f8bc3435cc8d39410d6a69572837a8
Filip <fihu08001@utb.vaxjo.se>
2025-11-04 12:13:37 +0100

    Add project files.

---
+++ uppgift 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace uppgift_2._1
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är din favoritbok?");
+            Console.Write("Din favoritbok är:");
+            Console.ReadLine();
+
+        }
+    }
+}
